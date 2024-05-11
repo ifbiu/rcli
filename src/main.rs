06af268f4cb187d6f8903a1f6184329ede0f905e
Candide <biuvip@163.com>
2024-05-11 18:56:53 +0800

    feature: add csv cli support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Welcome use candide rust cli!");
+// rcli csv -i input.csv -o output.json --header -d ','
+
+use clap::{Parser};
+use rcli::{Opts, process_csv, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    println!("{:?}",opts);
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
